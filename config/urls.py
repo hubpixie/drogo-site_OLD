bc58fa73543788069a994c5d4c597d3351c6fdcb
hubpixie <edor_pixie@hotmail.com>
2020-09-27 17:01:12 +0900

    Add a relation between REST framework and project URLconf.

--- config/urls.py
+++ config/urls.py
@@ -15,8 +15,10 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from sample_app.urls import router as drg_app_router
 
 urlpatterns = [
     path('', include('sample_app.urls')),
     path('admin/', admin.site.urls),
+    path('drg_api/', include(drg_app_router.urls)),
 ]
