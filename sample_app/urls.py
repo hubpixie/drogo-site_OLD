f7224507daf501df787a881c6f6862d36c9b1801
hubpixie <edor_pixie@hotmail.com>
2020-09-27 16:54:50 +0900

    Add a relation between REST framework and app URLconf.

--- sample_app/urls.py
+++ sample_app/urls.py
@@ -1,6 +1,11 @@
 from django.urls import path
-from sample_app.views import *
+from sample_app.views.root_view import *
+from sample_app.views.memo_viewset import *
+from rest_framework import routers
 
 urlpatterns = [
     path('', RootView.index, name='index'),
 ]
+
+router = routers.DefaultRouter()
+router.register(r'memo', MemoViewSet)
