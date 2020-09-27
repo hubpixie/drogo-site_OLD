42f7fec6bed2196c9ea433cbc5e4b3092129080a
hubpixie <edor_pixie@hotmail.com>
2020-09-28 00:14:43 +0900

    Bind swagger settings to project URLConf.

--- config/urls.py
+++ config/urls.py
@@ -15,10 +15,36 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from rest_framework import permissions
+from drf_yasg.views import get_schema_view
+from drf_yasg import openapi
 from sample_app.urls import router as drg_app_router
 
+schema_view = get_schema_view(
+   openapi.Info(
+      title="Snippets API",
+      default_version='v1',
+      description="Test description",
+      terms_of_service="https://www.google.com/policies/terms/",
+      contact=openapi.Contact(email="contact@snippets.local"),
+      license=openapi.License(name="BSD License"),
+   ),
+   public=True,
+   permission_classes=(permissions.AllowAny,),
+)
+# schema_view = get_swagger_view(title='API Lists')
+
+# urlpatterns = [
+#     path('', include('sample_app.urls')),
+#     path('admin/', admin.site.urls),
+#     path('drg_api/', include(drg_app_router.urls)),
+# ]
+
 urlpatterns = [
-    path('', include('sample_app.urls')),
-    path('admin/', admin.site.urls),
-    path('drg_api/', include(drg_app_router.urls)),
+   path('', include('sample_app.urls')),
+   path('admin/', admin.site.urls),
+   path('drg_api/', include(drg_app_router.urls)),
+#    path('swagger(?P<format>\.json|\.yaml)', schema_view.without_ui(cache_timeout=0), name='schema-json'),
+   path('swagger/', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
+   path('redoc/', schema_view.with_ui('redoc', cache_timeout=0), name='schema-redoc'),
 ]
