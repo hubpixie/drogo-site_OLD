e34e51e15f83e75e7345320b18f0ecd6962d678f
hubpixie <edor_pixie@hotmail.com>
2020-09-28 23:35:15 +0900

    Bind swagger settings to project URLConf. (Fix)

--- config/urls.py
+++ config/urls.py
@@ -15,6 +15,7 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from django.conf.urls import url
 from rest_framework import permissions
 from drf_yasg.views import get_schema_view
 from drf_yasg import openapi
@@ -45,6 +46,9 @@ urlpatterns = [
    path('admin/', admin.site.urls),
    path('drg_api/', include(drg_app_router.urls)),
 #    path('swagger(?P<format>\.json|\.yaml)', schema_view.without_ui(cache_timeout=0), name='schema-json'),
-   path('swagger/', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
-   path('redoc/', schema_view.with_ui('redoc', cache_timeout=0), name='schema-redoc'),
+#    path('swagger/', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
+#    path('redoc/', schema_view.with_ui('redoc', cache_timeout=0), name='schema-redoc'),
+   url(r'^swagger(?P<format>\.json|\.yaml)$', schema_view.without_ui(cache_timeout=0), name='schema-json'),
+   url(r'^swagger/$', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
+   url(r'^redoc/$', schema_view.with_ui('redoc', cache_timeout=0), name='schema-redoc'),   
 ]
